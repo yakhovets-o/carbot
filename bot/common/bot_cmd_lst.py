0b72db1b0102711b496f5ff886f455d6d78886a3
yakhovets-o <yakhovetso@gmail.com>
2024-03-05 00:01:05 +0300

    syntax correction

--- bot/common/bot_cmd_lst.py
+++ bot/common/bot_cmd_lst.py
@@ -7,5 +7,4 @@ bot_cmd_lst = [
     BotCommand(command='sub', description='Оплата подписки'),
     BotCommand(command='begin', description='Параметры поиска'),
     BotCommand(command='help', description='Список команд')
-
 ]
